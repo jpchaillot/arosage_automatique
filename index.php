12f02da0d5e08e2eb71f8fd5403d588a50acd821
jpchaillot <jpchaillot@gmail.com>
2021-06-21 21:54:48 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Contrôle GPIO</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css" /link>
+    </head>
+    <body>
+        <form action="script.php" method="post">
+            <input type="submit" name="executer" value="ON" class="button" id="ON">
+            <br/>
+            <input type="submit" name="executer" value="OFF" class="button" id="OFF">
+        </form>
+    </body>
+</html>
