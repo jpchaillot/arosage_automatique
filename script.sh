b148a3fc501a97af252ece3aae197fb46488c020
jpchaillot <jpchaillot@gmail.com>
2021-06-22 17:02:24 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,8 @@ sudo apt-get upgrade -y
 
 git clone https://github.com/jpchaillot/arosage_automatique.git /var/www/html
 
+sudo apt install apache2 php php-mbstring   && sudo chown -R pi:www-data /var/www/html/ &&  sudo chmod -R 777 /var/www/html/
+
 
 sudo apt-get install git-core -y
 #drogon.net/wiringPi
